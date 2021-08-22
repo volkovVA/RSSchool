b2c713f5e7d3d582179e68dbe05f93c56a443898
Viacheslav Volkov <volkov.slava@hotmail.com>
2021-08-22 14:59:14 +0300

    feat: add player layout and styles

---
+++ custom-video-player/script.js
@@ -0,0 +1,13 @@
+function playerProgress() {
+  const playerControl = document.querySelector(".player__control");
+
+  playerControl.addEventListener("input", e => {
+    const target = e.target
+    if (target.classList.contains('player__progress')) {
+      const value = target.value;
+      target.style.background = `linear-gradient(to right, #24809e 0%, #24809e ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`;
+    }
+  });
+}
+
+playerProgress();
