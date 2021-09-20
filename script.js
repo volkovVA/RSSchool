b79ab46967f0099ccc402c048c931ac33b9b27b4
volkovVA <volkov.slava@hotmail.com>
2021-09-20 20:39:13 +0300

    feat: nav & burger-menu

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", startUp);
+window.addEventListener("load", activateBurgerMenu);
 
 function startUp() {
   const color = document.querySelector("#color");
@@ -11,4 +12,34 @@ function startUp() {
   color.addEventListener("input", changeColor);
   color.addEventListener("change", changeColor);
   color.select();
+}
+
+function activateBurgerMenu() {
+  const burgerBtn = document.querySelector('.nav__icon');
+  const nav = document.querySelector('.nav');
+  const navItem = document.querySelectorAll('.nav__item');
+
+  const removeActive = () => {
+    nav.classList.remove('active');
+    document.body.style.overflow = '';
+  };
+
+  const addActive = () => {
+    nav.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  };
+
+  burgerBtn.addEventListener('click', () => {
+    if (nav.classList.contains('active')) {
+      removeActive();
+    } else {
+      addActive();
+    }
+  });
+
+  navItem.forEach((item) => {
+    item.addEventListener('click', () => {
+      removeActive();
+    });
+  });
 }
